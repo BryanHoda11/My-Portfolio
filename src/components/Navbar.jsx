1f0647e9bbb90a82c5b928bf4748eee243d4f0b3
Bryan Hoda <bryanhoda2003@gmail.com>
2025-07-31 14:05:49 +0530

    Responsive UI fix in Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,8 +12,8 @@ const [link, setlink] = useState(false);
     return (
         <>
             <nav className='sticky top-0 w-full bg-white z-50'>
-              <div className='flex items-center justify-between md:px-5 py-4 shadow-md shadow-blue-200'>
-                <div className="logo cursor-default text-blue-500 font-bold logo-font text-lg sm:text-xl flex flex-col md:flex-row items-center">Hi, This is Bryan <span className='flex items-center text-xs sm:text-sm gap-2 text-gray-800 opacity-80 px-6 pt-1 md:pt-0'><IoLocationSharp style={{fontSize: '18px'}}/> Mumbai , Maharashtra - India</span></div> 
+              <div className='flex items-center justify-between md:px-5 py-4 shadow-md shadow-blue-200 px-3'>
+                <div className="logo cursor-default text-blue-500 font-bold logo-font text-lg sm:text-xl flex flex-col md:flex-row md:items-center">Hi, This is Bryan <span className='flex items-center text-xs sm:text-sm text-gray-800 opacity-70 md:px-6'><IoLocationSharp style={{fontSize: '18px'}}/> Mumbai , Maharashtra - India</span></div> 
 
                 <div className='links hidden lg:flex items-center gap-10'>
                   <a href='https://github.com/BryanHoda11' target='_blank' rel='noopener noreferrer' className='cursor-pointer text-gray-600 hover:text-blue-500 transition-all duration-500'><FaGithub style={{fontSize: '20px'}}/></a>
@@ -21,7 +21,7 @@ const [link, setlink] = useState(false);
                   <a href='https://www.instagram.com/bryaannnn.11/' target='_blank' className='cursor-pointer text-gray-600 hover:text-blue-500 transition-all duration-500' hover:text-blue-500 transition-all duration-200><FaInstagram style={{fontSize: '20px'}}/></a> 
                 </div>
 
-                {<div className="block lg:hidden toggle text-gray-600 px-4 cursor-pointer">
+                {<div className="block lg:hidden toggle text-gray-600 cursor-pointer">
                     {link ? (<MdOutlineKeyboardDoubleArrowDown className='text-blue-500' onClick={ () => setlink(!link) } style={{fontSize: '23px'}}/>) : (<MdOutlineKeyboardDoubleArrowLeft onClick={ () => setlink(!link) } style={{fontSize: '23px'}}/>)}
                 </div>}
 
@@ -34,7 +34,6 @@ const [link, setlink] = useState(false);
                 </div>}
               </div>
             </nav>
-           
         </>
     )
 }
